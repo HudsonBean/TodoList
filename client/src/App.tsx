9fb73222e9691803bfa9faaf5e94d217e3f6b64c
Hudson <hudsonbeandev@gmail.com>
2023-08-12 12:08:25 -0500

    created client

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <h1>Hello World!</h1>
+  </React.StrictMode>
+);
