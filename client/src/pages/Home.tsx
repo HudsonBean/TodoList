7e516c12e71914cf74215bfcdb580408ceb39151
Hudson <hudsonbeandev@gmail.com>
2023-08-12 12:21:22 -0500

    Set up api

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { useGetAllTodosQuery } from "../features/apiSlice";
 
 export const Home = () => {
+  const { data, error, isLoading } = useGetAllTodosQuery(undefined);
+  console.log(data, error, isLoading);
   return <div>Home</div>;
 };
