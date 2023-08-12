1d194205cd46de84fa4d5302acd41f62f92a8dc8
Hudson <hudsonbeandev@gmail.com>
2023-08-12 12:13:43 -0500

    hooks and setup pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./store";
+//Pages
+import { Home } from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
