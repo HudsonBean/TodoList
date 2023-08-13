fcbec42511c9f8640c31be6f16fcef9acf0a3d0d
Hudson <hudsonbeandev@gmail.com>
2023-08-13 01:22:51 -0500

    Created custom button component

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,6 +1,57 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import "../styles/Button.css";
 
-export const Button = () => {
-  return <button></button>;
+interface Props {
+  onClick: () => void;
+  children?: React.ReactNode;
+  width: string;
+  height: string;
+  color: string;
+  backgroundColor: string;
+  borderRadius: string;
+  stopPropogation: boolean;
+  preventDefault: boolean;
+}
+
+const clickHanlder = (
+  onClick: Function,
+  stopPropagation: boolean,
+  preventDefault: boolean
+) => {
+  return (e: MouseEvent) => {
+    //Stop propogation
+    if (stopPropagation) {
+      e.stopPropagation();
+    }
+    //PreventDefault
+    if (preventDefault) {
+      e.preventDefault();
+    }
+    onClick();
+  };
 };
+
+export const Button: React.FC<Props> = ({
+  onClick,
+  children,
+  width,
+  height,
+  color,
+  backgroundColor,
+  borderRadius,
+  stopPropogation,
+  preventDefault,
+}) => (
+  <button
+    onClick={clickHanlder(onClick, stopPropogation, preventDefault)}
+    style={{
+      width: width,
+      height: height,
+      color: color,
+      backgroundColor: backgroundColor,
+      borderRadius: borderRadius,
+    }}
+  >
+    {children}
+  </button>
+);
