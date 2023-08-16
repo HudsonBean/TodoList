098b8ed660265a11abcfd2303982a1ff3068d616
Hudson <hudsonbeandev@gmail.com>
2023-08-16 06:32:26 -0500

    go to school

--- client/src/components/Template/NavBar/CreateTodoList.tsx
+++ client/src/components/Template/NavBar/CreateTodoList.tsx
@@ -2,28 +2,62 @@ import React from "react";
 import { Button } from "../../Button";
 import { useCreateTodoMutation } from "../../../features/apiSlice";
 
-const createNewListELement: React.FC<undefined> = () => {
-  return <form></form>;
-};
-
 {
-  /* <li onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
-  <div className="todo-lists__data-wrapper">
-    <div className="todo-lists__asthetics-wrapper">
-      <span className="material-symbols-outlined todo-lists__todo-list-icon">
-        checklist
-      </span>
-      <span className="todo-lists__todo-list-name">TodoList1</span>
-    </div>
-    <span className="todo-lists__todo-list-date">August 5, 2023, 1:15 AM</span>
-  </div>
-  <span className="material-symbols-outlined todo-lists__more-icon hidden">
-    more_vert
-  </span>
-</li>; */
+  //    <li onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
+  //   <div className="todo-lists__data-wrapper">
+  //     <div className="todo-lists__asthetics-wrapper">
+  //       <span className="material-symbols-outlined todo-lists__todo-list-icon">
+  //         checklist
+  //       </span>
+  //       <span className="todo-lists__todo-list-name">TodoList1</span>
+  //     </div>
+  //     <span className="todo-lists__todo-list-date">August 5, 2023, 1:15 AM</span>
+  //   </div>
+  //   <span className="material-symbols-outlined todo-lists__more-icon hidden">
+  //     more_vert
+  //   </span>
+  // </li>;
 }
 
 export const CreateTodoList = () => {
+  const mouseEnterHandler = (e: React.MouseEvent | any) => {
+    const element: HTMLElement = e.currentTarget.getElementsByClassName(
+      "todo-lists__more-icon"
+    )[0];
+    element.classList.remove("hidden");
+  };
+  const mouseLeaveHandler = (e: React.MouseEvent | any) => {
+    const element: HTMLElement = e.currentTarget.getElementsByClassName(
+      "todo-lists__more-icon"
+    )[0];
+    element.classList.add("hidden");
+  };
+  const createNewListELement: React.FC<undefined> = () => {
+    return (
+      <form
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          console.log(e);
+        }}
+      >
+        <li onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
+          <div className="todo-lists__data-wrapper">
+            <div className="todo-lists__asthetics-wrapper">
+              <span className="material-symbols-outlined todo-lists__todo-list-icon">
+                checklist
+              </span>
+              <input type="text" required={true}></input>
+            </div>
+            <span className="todo-lists__todo-list-date">
+              August 5, 2023, 1:15 AM
+            </span>
+          </div>
+          <span className="material-symbols-outlined todo-lists__more-icon hidden">
+            more_vert
+          </span>
+        </li>
+      </form>
+    );
+  };
   return (
     <div className="nav-bar__create-todo-list">
       <Button
--- client/src/components/Template/NavBar/TodoLists.tsx
+++ client/src/components/Template/NavBar/TodoLists.tsx
@@ -1,36 +1,5 @@
 import React from "react";
 
 export const TodoLists = () => {
-  const mouseEnterHandler = (e: React.MouseEvent | any) => {
-    const element: HTMLElement = e.currentTarget.getElementsByClassName(
-      "todo-lists__more-icon"
-    )[0];
-    element.classList.remove("hidden");
-  };
-  const mouseLeaveHandler = (e: React.MouseEvent | any) => {
-    const element: HTMLElement = e.currentTarget.getElementsByClassName(
-      "todo-lists__more-icon"
-    )[0];
-    element.classList.add("hidden");
-  };
-  return (
-    <ul className="nav-bar__todo-lists">
-      <li onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
-        <div className="todo-lists__data-wrapper">
-          <div className="todo-lists__asthetics-wrapper">
-            <span className="material-symbols-outlined todo-lists__todo-list-icon">
-              checklist
-            </span>
-            <span className="todo-lists__todo-list-name">TodoList1</span>
-          </div>
-          <span className="todo-lists__todo-list-date">
-            August 5, 2023, 1:15 AM
-          </span>
-        </div>
-        <span className="material-symbols-outlined todo-lists__more-icon hidden">
-          more_vert
-        </span>
-      </li>
-    </ul>
-  );
+  return <ul className="nav-bar__todo-lists"></ul>;
 };
